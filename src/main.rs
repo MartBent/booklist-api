130dbad37b6645ee50be38c7ab3c5d94fc172fae
Mart Bent <martbent99@gmail.com>
2023-03-20 20:39:05 +0100

    Added json decoding

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,41 @@
 use actix_web::{get, App, HttpServer, Responder};
 use serde::{Deserialize, Serialize};
+use std::fs;
 use std::io::Result;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Book {
+    pub id: u16,
     pub title: String,
     pub page_amount: u16,
 }
 
-pub fn get_books() -> Vec<Book> {
-    let mut book_list: Vec<Book> = Vec::new();
-    book_list.push(Book::new(String::from("Misery"), 261));
-    book_list.push(Book::new(String::from("Misery"), 261));
-    book_list.push(Book::new(String::from("Lord of the flies"), 210));
-    book_list.push(Book::new(
-        String::from("Harry Potter: The chamber of secrets"),
-        513,
-    ));
-    book_list.push(Book::new(String::from("Vals alarm"), 400));
-    book_list
-}
-
 impl Book {
-    pub fn new(title: String, page_amount: u16) -> Self {
+    pub fn new(id: u16, title: String, page_amount: u16) -> Self {
         Self {
-            title: title,
-            page_amount: page_amount,
+            id,
+            title,
+            page_amount,
         }
     }
 }
 
+pub fn get_books() -> Result<Vec<Book>> {
+    let json_string = fs::read_to_string("./books.json")?;
+    let mut book_list: Vec<Book> = serde_json::from_str(&json_string)?;
+    book_list.push(Book { id: 2, title: String::from("The lord of the flies"), page_amount: 196 });
+    Ok(book_list)
+}
+
 #[get("/books")]
 async fn books() -> impl Responder {
-    let res_body = serde_json::to_string(&get_books()).unwrap();
+    let res_body = match get_books() {
+        Ok(book_list) => match serde_json::to_string(&book_list) {
+            Ok(book_list_json_string) => book_list_json_string,
+            Err(_) => String::from("Error decoding JSON file"),
+        },
+        Err(_) => String::from("{\"Error:\": \"Error fetching books from file\"}"),
+    };
     res_body
 }
 
